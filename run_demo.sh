78c1fc012398c193c5804d52422856c2d83e2bde
Wei Li <wei_li@login3.colonialone.gwu.edu>
2018-09-21 14:57:17 -0400

    fix demo

--- run_demo.sh
+++ run_demo.sh
@@ -7,6 +7,11 @@
 #$ -pe pvm 4
 #$ -cwd
 
+# step 0: before running this demo, go to the demo folder, 
+# and make sure all the reference genome sequence and annotation 
+# is properly downloaded
+# refer to prepare_demo.sh to download these files
+
 # step 1: generate reference index
 python generate_ref.py metadata/config.yaml 
 
