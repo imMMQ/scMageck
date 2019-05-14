1c5b4d78d567a06b6cddef4e62ae0ec38ea4def3
Wei Li <davidliwei@bitbucket.org>
2019-05-14 12:48:45 -0400

    revising demo command

--- demo/demo1_scmageck_R/run_scmageck_rra.sh
+++ demo/demo1_scmageck_R/run_scmageck_rra.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-Rscript ../../tools/R/run_individual_genes.R --RDS singles_dox_mki67.RDS  --LIBRARY sgrna_lib.txt  --BARCODE barcode_rec.txt  --GENE MKI67 --LABEL dox_mki67
+Rscript ../../tools/R/run_individual_genes.R --RDS singles_dox_mki67.RDS    --BARCODE barcode_rec.txt  --GENE MKI67 --LABEL dox_mki67
