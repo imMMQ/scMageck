b35f5151acc76f52b2500b9a691e54e2866a05c8
Wei Li <li.david.wei@gmail.com>
2019-09-10 13:41:18 -0400

    revising the bash shell script in demo folder

--- demo/demo1_scmageck_R/run_scmageck_lr_v3.sh
+++ demo/demo1_scmageck_R/run_scmageck_lr_v3.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# make sure to install scmageck and R correctly before running this script
 
-Rscript ../../tools/R/run_lr.R --RDS singles_dox_mki67_v3.RDS  --LIBRARY sgrna_lib.txt  --BARCODE barcode_rec.txt   --LABEL dox_scmageck_lr --NEGCTRL NonTargetingControlGuideForHuman --PERMUTATION 1000
+Rscript scmageck_lr_demo.R
+
+# unofficial use of R functions
+#Rscript ../../tools/R/run_lr.R --RDS singles_dox_mki67_v3.RDS  --LIBRARY sgrna_lib.txt  --BARCODE barcode_rec.txt   --LABEL dox_scmageck_lr --NEGCTRL NonTargetingControlGuideForHuman --PERMUTATION 1000
--- demo/demo1_scmageck_R/run_scmageck_rra_v3.sh
+++ demo/demo1_scmageck_R/run_scmageck_rra_v3.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 
-Rscript ../../tools/R/run_individual_genes.R --RDS singles_dox_mki67_v3.RDS  --LIBRARY sgrna_lib.txt  --BARCODE barcode_rec.txt  --GENE MKI67 --LABEL dox_mki67
+# make sure to install scmageck and R correctly before running this script
+Rscript scmageck_rra_demo.R
+
+
+# unofficial use of R functions
+#Rscript ../../tools/R/run_individual_genes.R --RDS singles_dox_mki67_v3.RDS  --LIBRARY sgrna_lib.txt  --BARCODE barcode_rec.txt  --GENE MKI67 --LABEL dox_mki67
+~
