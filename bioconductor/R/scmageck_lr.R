7f0ea50884ba96b7e7cd6c7dc912bdcc639e00a0
Lin Yang <yl2018nuli@outlook.com>
2020-01-11 19:02:09 +0800

    no message

--- bioconductor/R/scmageck_lr.R
+++ bioconductor/R/scmageck_lr.R
@@ -80,10 +80,6 @@ scmageck_lr <- function(BARCODE, RDS, NEGCTRL, SELECT_GENE = NULL, LABEL = NULL,
   
   # Optional function
   # Get the results based on gmt file
-<<<<<<< HEAD
-  if (!is.null(data_signature)) {
-    gmt <- read.delim(data_signature, header = FALSE)
-=======
   if(!is.null(data_signature)){
     x <- scan(data_signature, what = "", sep = "\n")
     x <- strsplit(x, "\t") # split string by white space
@@ -91,7 +87,6 @@ scmageck_lr <- function(BARCODE, RDS, NEGCTRL, SELECT_GENE = NULL, LABEL = NULL,
     cn <- paste("V", 1:max.col, sep = "")
     gmt <- read.table(data_signature, fill = TRUE, col.names = cn)
     # gmt <- read.delim(data_signature, header = FALSE)
->>>>>>> 9dffa0b87c82f72e5ce5b0b44f231edaa51f957b
     gmt <- t(as.matrix(gmt))
     colnames(gmt) <- gmt[1, ]
     gmt <- gmt[-1:-2, ]
