c84fa3dcc0d4a2b884c21ee7958e9e37280a609e
Wei Li <li.david.wei@gmail.com>
2020-02-05 10:15:29 -0500

    change demo1/run_scmageck_rra_v3.sh; rename GSEA demo

--- demo/demo1_scmageck_R/run_scmageck_rra_v3.sh
+++ demo/demo1_scmageck_R/run_scmageck_rra_v3.sh
@@ -7,4 +7,3 @@ Rscript scmageck_rra_demo.R
 
 # unofficial use of R functions
 #Rscript ../../tools/R/run_individual_genes.R --RDS singles_dox_mki67_v3.RDS  --LIBRARY sgrna_lib.txt  --BARCODE barcode_rec.txt  --GENE MKI67 --LABEL dox_mki67
-~
